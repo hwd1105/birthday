49924e378987b192b2e54ae6dd72a0bd64eed9e7
hwd1105 <57223991+hwd1105@users.noreply.github.com>
2019-10-31 22:42:26 +0800

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,19 +1,38 @@
-// 霸都丶傲天 2019.10.10
-$(function () {
-    let dom = document.createElement("span");
-    config.texts.forEach(function (item) {
-        let p = document.createElement("p");
-        p.innerHTML = item;
-        dom.appendChild(p);
-    });
-    $("#texts-container").append(dom.innerHTML);
-});
-
-$(function () {
-    for (let k in config.desc) {
-        let dom = $("#" + k);
-        if (dom.length > 0 && config.desc[k]) {
-            dom.html(config.desc[k]);
-        }
+//MrHWD  2019年10月10日 https://github.com/bigdigjun/birthday
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "送给张童鞋",
+        "心爱的小可爱",
+        "今天是你的生日",
+        "这是你在福清的",
+        "第三个生日了哦",
+        "每一年",
+        "都有这样一个特殊的日子",
+        "完全属于你",
+        "祝你生日快乐!",
+        "希望你每天醒来",
+        "都是阳光的",
+        "不会因为别人的几句话",
+        "几个表情和几个举止",
+        "影响自己的心情",
+        "好好生活",
+        "总会遇见美好的事",
+        "生活的温柔",
+        "都会哒哒哒的",
+        "跑进你的怀里呐",
+        "FROM~~韩先森"
+    ],
+    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    desc: {
+        turn_on: "开始",
+        play: "音乐",
+        bannar_coming: "颜色",
+        balloons_flying: "好像少点东西",
+        cake_fadein: "蛋糕？",
+        light_candle: "蜡烛？",
+        wish_message: "生日快乐",
+        story: "A MESSAGE FOR YOU",
     }
-});
+};
